b71492b6a32123f949feb7adffbb84cdf22d675a
bnowacki <bknowacki@gmail.com>
2023-10-20 22:06:23 +0200

    fix

--- web/src/pages/expenses/index.tsx
+++ web/src/pages/expenses/index.tsx
@@ -1,117 +1,104 @@
-import {
-  Box,
-  Tabs,
-  Text,
-  TabList,
-  Tab,
-  TabPanels,
-  TabPanel,
-  List,
-  ListItem,
-  ListIcon,
-  Button,
-  Flex,
-} from "@chakra-ui/react";
-import { CalendarIcon } from "@chakra-ui/icons";
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer, Label } from "recharts";
-import { useState } from "react";
-import { format } from "date-fns";
-import { dayDateFormat, weekDateFormat, monthDateFormat, yearDateFormat } from "@/utils/string";
+import {useState} from 'react'
+
+import {CalendarIcon} from '@chakra-ui/icons'
+import {Box, Button, Flex, List, ListIcon, ListItem, Text} from '@chakra-ui/react'
+import {format} from 'date-fns'
+import {Cell, Label, Pie, PieChart, ResponsiveContainer} from 'recharts'
+
+import {dateFormat, dayDateFormat, monthDateFormat, yearDateFormat} from '@/utils/string'
 
 const data = [
-  { name: "Group A", value: 400 },
-  { name: "Group B", value: 300 },
-  { name: "Group C", value: 300 },
-  { name: "Group D", value: 200 },
-];
+  {name: 'Group A', value: 400},
+  {name: 'Group B', value: 300},
+  {name: 'Group C', value: 300},
+  {name: 'Group D', value: 200},
+]
 
-const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042']
 
 type DateRange = 'day' | 'week' | 'month' | 'year'
 
 const buttons: DateRange[] = ['day', 'week', 'month', 'year']
 
-const dateFormats: { [key in DateRange]: string } = {
-    day: dayDateFormat,
-    week: weekDateFormat,
-    month: monthDateFormat,
-    year: yearDateFormat,
-};
+const dateFormats: {[key in DateRange]: string} = {
+  day: dayDateFormat,
+  week: dateFormat,
+  month: monthDateFormat,
+  year: yearDateFormat,
+}
 
 const ExpensesPage = () => {
   const [active, setActive] = useState<DateRange>('day')
-  const [currentDate, setCurrentDate] = useState<Date>(new Date());
 
   const formatDate = (value: Date | string | null) =>
-    value ? format(new Date(value), dateFormats[active]) : "";
+    value ? format(new Date(value), dateFormats[active]) : ''
 
   return (
     <Box position="relative" width={'100%'} height={'100%'} p={0}>
       <Flex>
-        {buttons.map(b => <Button 
-          key={b}
-          onClick={() => setActive(b)}
-          color={active === b ? 'red' : 'black'}
-          borderBottom={active === b ? "2px solid red" : "2px solid transparent"}
-          bg="transparent"
-          boxShadow='none'
-          _hover={{bg:"white"}}
-          _active={{ bg: "white", boxShadow: "none" }}
-          flex='1'
-          rounded='none'
-
-        >{b}</Button>)}
-        </Flex>
-        <Text
-          textAlign="center"
-          mt="10px"
-          fontSize="22px"
-          style={{ textDecoration: "underline" }}
+        {buttons.map((b) => (
+          <Button
+            key={b}
+            onClick={() => setActive(b)}
+            color={active === b ? 'red' : 'black'}
+            borderBottom={active === b ? '2px solid red' : '2px solid transparent'}
+            bg="transparent"
+            boxShadow="none"
+            _hover={{bg: 'white'}}
+            _active={{bg: 'white', boxShadow: 'none'}}
+            flex="1"
+            rounded="none"
+          >
+            {b}
+          </Button>
+        ))}
+      </Flex>
+      <Text
+        textAlign="center"
+        mt="10px"
+        fontSize="22px"
+        style={{textDecoration: 'underline'}}
         align="center"
         mb="20px"
-        >
-          {formatDate(currentDate)}
-        </Text>
+      >
+        {formatDate(new Date())}
+      </Text>
       <ResponsiveContainer>
-          <PieChart  width={280} height={280}>
-            <Pie
-              data={data}
-              cy={100}
-              innerRadius={80}
-              outerRadius={105}
-              fill="#8884d8"
-              paddingAngle={2}
-              dataKey="value"
-            >
-              {data.map((entry, index) => (
-                <Cell
-                  key={`cell-${index}`}
-                  fill={COLORS[index % COLORS.length]}
-                />
-              ))}
-
-              <Label
-                value="2500$"
-                position="center"
-                fill="grey"
-                style={{
-                  fontSize: "30px",
-                  fontWeight: "bold",
-                  fontFamily: "Roboto",
-                }}
-              />
-            </Pie>
+        <PieChart width={280} height={280}>
+          <Pie
+            data={data}
+            cy={100}
+            innerRadius={80}
+            outerRadius={105}
+            fill="#8884d8"
+            paddingAngle={2}
+            dataKey="value"
+          >
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+            <Label
+              value="2500$"
+              position="center"
+              fill="grey"
+              style={{
+                fontSize: '30px',
+                fontWeight: 'bold',
+                fontFamily: 'Roboto',
+              }}
+            />
+          </Pie>
         </PieChart>
-        </ResponsiveContainer>
+      </ResponsiveContainer>
 
       <List color="white" fontSize="1.2em" spacing={4} p="20px">
-          <ListItem>
+        <ListItem>
           <ListIcon as={CalendarIcon} color="white"></ListIcon>
           <Text>DashBoard</Text>
-          </ListItem>
-        </List>
-        </Box>
-  );
-};
+        </ListItem>
+      </List>
+    </Box>
+  )
+}
 
-export default ExpensesPage;
+export default ExpensesPage
--- web/src/utils/string.ts
+++ web/src/utils/string.ts
@@ -3,26 +3,20 @@ import _ from 'lodash'
 
 export const sentenceCase = (input: string) => _.startCase(_.toLower(input))
 
-export const dateFormat = 'dd  MMM | eeee'
+export const transactionDateFormat = 'dd  MMM | eeee'
 export const formatTransactionDate = (value?: Date | string | null) =>
-  value ? format(new Date(value), dateFormat) : ''
+  value ? format(new Date(value), transactionDateFormat) : ''
 
-export const polishDateFormat = 'dd.MM.yyyy'
-export const formatDate = (value?: Date | string | null) =>
-  value ? format(new Date(value), polishDateFormat) : ''
+export const dateFormat = 'dd.MM.yyyy'
+export const formatDate = (value?: Date | string | null) => (value ? format(new Date(value), dateFormat) : '')
 
 export const polishTimestampFormat = 'dd.MM.yyyy HH:mm'
 export const formatTimestamp = (value: Date | string | null) =>
   value ? format(new Date(value), polishTimestampFormat) : ''
 
-<<<<<<< HEAD
 export const dayDateFormat = 'DDDD, dd MMMM'
 export const formatDayStats = (value: Date | string | null) =>
   value ? format(new Date(value), dayDateFormat) : ''
-
-export const weekDateFormat = 'dd.MM.yyyy'
-export const formatWeekStats = (value: Date | string | null) =>
-  value ? format(new Date(value), weekDateFormat) : ''
 
 export const monthDateFormat = 'MMMM'
 export const formatMonthStats = (value: Date | string | null) =>
@@ -32,6 +26,4 @@ export const yearDateFormat = 'yyyy'
 export const formatYearStats = (value: Date | string | null) =>
   value ? format(new Date(value), yearDateFormat) : ''
 
-=======
 export const formatMoney = (v?: number | null) => ((v || 0) / 100).toFixed(2)
->>>>>>> 2f0358fecc72dc83d7473e72c6e0c6ad6884c2c3
