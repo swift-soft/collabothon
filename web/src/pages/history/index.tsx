d281243b68edc67b704061633328ad4cffa751b4
247039 Tymoteusz Piwowarski <247039@edu.p.lodz.pl>
2023-10-20 18:14:20 +0200

    history

--- web/src/pages/history/index.tsx
+++ web/src/pages/history/index.tsx
@@ -1,7 +1,92 @@
 import React from 'react'
 
+import {ArrowBackIcon, HamburgerIcon, InfoOutlineIcon} from '@chakra-ui/icons'
+import {Box, Button, Flex, HStack, Spacer, Stack, Text} from '@chakra-ui/react'
+
+import {formatTransactionDate} from '@/utils/string.ts'
+
+const transactions = [
+  {
+    id: 'hsdaido',
+    amount: 1200,
+    created_at: new Date(),
+    name: 'Kaufland',
+  },
+  {
+    id: 'hsdaido',
+    amount: 212300,
+    created_at: new Date(),
+    name: 'Apart',
+  },
+  {
+    id: 'hsdaido',
+    amount: 31052,
+    created_at: new Date(),
+    name: 'McDonald',
+  },
+  {
+    id: 'hsdaido',
+    amount: 4214,
+    created_at: new Date(),
+    name: 'Dino',
+  },
+  {
+    id: 'hsdaido',
+    amount: 12423,
+    created_at: new Date(),
+    name: 'KFC',
+  },
+  {
+    id: 'hsdaido',
+    amount: 53466,
+    created_at: new Date(),
+    name: 'Vape-shop',
+  },
+]
+
 const HistoryPage = () => {
-  return <div>HistoryPage</div>
+  return (
+    <Box>
+      <Flex p='2'>
+        <Box color="red" textAlign="center"> 
+          <ArrowBackIcon w={5} h={5}></ArrowBackIcon>
+          <Text fontSize="10px">Back</Text>
+        </Box>
+        <Spacer></Spacer>
+        <Box color="red">
+          <Text fontSize="20px" fontWeight="bold">Konto <InfoOutlineIcon fontSize="16px"></InfoOutlineIcon></Text>
+        </Box>
+        <Spacer></Spacer>
+        <Box color="red" textAlign="center">
+          <HamburgerIcon w={5} h={5}></HamburgerIcon>
+          <Text fontSize="10px" >Menu</Text>
+        </Box>
+      </Flex>
+
+      <Stack bgColor="#ededed" padding="10px 14px">
+        <Text fontWeight="bold" fontSize="22px">
+          Konto Commerzbank
+        </Text>
+        <Text float="left">6302,21 PLN</Text>
+      </Stack>
+      {transactions.map((t) => (
+        <Box key={t.id}>
+          <Box borderBottom="1px" borderColor="lightgray" padding="10px 14px">
+            <HStack justify="space-between">
+              <Stack>
+                <HStack color="red" fontWeight="bold">
+                  <Text textTransform="uppercase">{formatTransactionDate(t.created_at)}</Text>
+                </HStack>
+                <Text textTransform="uppercase">{t.name}</Text>
+              </Stack>
+              <Text></Text>
+              <Text>{((t.amount / 100) * -1).toFixed(2)} PLN</Text>
+            </HStack>
+          </Box>
+        </Box>
+      ))}
+    </Box>
+  )
 }
 
 export default HistoryPage
