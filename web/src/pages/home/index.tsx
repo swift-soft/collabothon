6dc4431f2c2f47de9942f4305b5306fd4b9ed987
KMuszynski <m.krzysiek2003@gmail.com>
2023-10-20 19:31:12 +0200

    done

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -1,21 +1,19 @@
-import { Box } from "@chakra-ui/react";
-import Hero from "./sections/hero";
-import Links from "./sections/links";
-import Operations from "./sections/operations";
-import Navbar from "./sections/navBar";
+import {Box} from '@chakra-ui/react'
+
+import Hero from './sections/hero'
+import Links from './sections/links'
+import Navbar from './sections/navBar'
+import Operations from './sections/operations'
 
 const HomePage = () => {
   return (
-    <div style={{ height: "100%" }}>
+    <Box h="100%">
       <Navbar />
       <Hero />
       <Links />
       <Operations />
-      <Box width="100%" height="40px" bg="red.800">
-        Bottom Navbar !!!
-      </Box>
-    </div>
-  );
-};
+    </Box>
+  )
+}
 
 export default HomePage
--- web/src/pages/home/sections/hero/index.tsx
+++ web/src/pages/home/sections/hero/index.tsx
@@ -1,31 +1,14 @@
-import {
-  Box,
-  Stack,
-  Heading,
-  Text,
-  Link,
-  Flex,
-  Button,
-} from "@chakra-ui/react";
+import {Box, Button, Flex, Heading, Link, Stack, Text} from '@chakra-ui/react'
 // import Carousel from "react-bootstrap/Carousel";
-import {
-  CarouselProvider,
-  Slider,
-  Slide,
-  ButtonBack,
-  ButtonNext,
-  Dot,
-} from "pure-react-carousel";
-import "pure-react-carousel/dist/react-carousel.es.css";
+import {ButtonBack, ButtonNext, CarouselProvider, Dot, Slide, Slider} from 'pure-react-carousel'
+import 'pure-react-carousel/dist/react-carousel.es.css'
+
+import './style.css'
 
 const Hero = () => {
   return (
     <Box height="fit-content">
-      <CarouselProvider
-        naturalSlideWidth={16}
-        naturalSlideHeight={9}
-        totalSlides={2}
-      >
+      <CarouselProvider naturalSlideWidth={16} naturalSlideHeight={9} totalSlides={2}>
         <Slider>
           <Slide index={0}>
             <Stack
@@ -40,14 +23,7 @@ const Hero = () => {
               marginLeft="0"
               height="100%"
             >
-              <Text
-                pl="6"
-                as="h2"
-                fontSize="md"
-                mb="0"
-                ml="0"
-                fontWeight="semibold"
-              >
+              <Text pl="6" as="h2" fontSize="md" mb="0" ml="0" fontWeight="semibold">
                 Available funds:
               </Text>
               <Text pl="10" fontSize="2xl" mb="2" mt="0" fontWeight="extrabold">
@@ -57,39 +33,15 @@ const Hero = () => {
                 <Text pl="6" fontSize="sm" fontWeight="medium" mb="0" mr="5px">
                   Expenses up to 20 April:
                 </Text>
-                <Text
-                  fontSize="sm"
-                  fontWeight="semibold"
-                  mb="0"
-                  color="yellow.700"
-                >
+                <Text fontSize="sm" fontWeight="semibold" mb="0" color="yellow.700">
                   5286,75 PLN
                 </Text>
               </Box>
               <Link>
-                <Text
-                  textAlign="center"
-                  fontSize="md"
-                  fontWeight="semibold"
-                  color="red.600"
-                >
+                <Text textAlign="center" fontSize="md" fontWeight="semibold" color="red.600">
                   YOUR EXPENSES
                 </Text>
               </Link>
-              <Flex justify="center" mt="auto" border="none">
-                <Dot slide={0} color="black">
-                  <Button bg="red.700" w="40px" h="10px" mr="2px" />
-                </Dot>
-                <Dot slide={1} color="black">
-                  <Button
-                    bg="red.200"
-                    w="40px"
-                    h="10px"
-                    className="carousel__dot--selected"
-                    ml="2px"
-                  />
-                </Dot>
-              </Flex>
             </Stack>
           </Slide>
           <Slide index={1}>
@@ -106,51 +58,27 @@ const Hero = () => {
               width="100%"
             >
               <h1>Recieve e-receipt</h1>
-              <Box
-                display="flex"
-                justifyContent="center"
-                width="100%"
-                margin="auto"
-              >
+              <Box display="flex" justifyContent="center" width="100%" margin="auto">
                 <img
                   src="https://www.freeiconspng.com/thumbs/barcode/barcode-background-png-6.png"
                   alt="Second slide"
                   width="50%"
                 />
               </Box>
-              <Flex justify="center" mt="auto" border="none">
-                <Dot slide={0} color="black">
-                  <Button bg="red.200" w="40px" h="10px" mr="2px" />
-                </Dot>
-                <Dot slide={1} color="black">
-                  <Button
-                    bg="red.700"
-                    w="40px"
-                    h="10px"
-                    className="carousel__dot--selected"
-                    ml="2px"
-                  />
-                </Dot>
-              </Flex>
             </Stack>
-            <Flex justify="center" mt="4">
-              <Dot slide={0} color="black">
-                <Button bg="red" w="40px" h="10px" />
-              </Dot>
-              <Dot slide={1} color="black">
-                <Button
-                  bg="red"
-                  w="40px"
-                  h="10px"
-                  className="carousel__dot--selected"
-                />
-              </Dot>
-            </Flex>
           </Slide>
         </Slider>
+        <Flex justify="center" mt={0} gap={4}>
+          <Dot slide={0} color="black">
+            <Button colorScheme="red" w="40px" h="10px" bg="red.200" />
+          </Dot>
+          <Dot slide={1} color="black">
+            <Button colorScheme="red" w="40px" h="10px" bg="red.200" />
+          </Dot>
+        </Flex>
       </CarouselProvider>
     </Box>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
