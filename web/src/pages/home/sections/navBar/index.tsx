d37776c286fb277bb8e23e7f3a4cb462f6841861
Krzysztof Muszyński <117378837+KMuszynski@users.noreply.github.com>
2023-10-20 20:23:38 +0200

    finished home page (#9)

--- web/src/pages/home/sections/navBar/index.tsx
+++ web/src/pages/home/sections/navBar/index.tsx
@@ -1,24 +1,17 @@
-import { Box, Text, Image } from "@chakra-ui/react";
+import {Box, Image, Text} from '@chakra-ui/react'
 
 const Navbar = () => {
   return (
-    <Box
-      display="flex"
-      flexDirection="row"
-      marginTop="2px"
-      pb="10px"
-      w="100%"
-      margin="auto"
-    >
+    <Box display="flex" flexDirection="row" marginTop="2px" pb="10px" w="100%" margin="auto">
       <Image
         src="https://assets.stickpng.com/images/588a6507d06f6719692a2d15.png"
-        alt="menue"
+        alt="menu"
         w="25px"
         h="25px"
         mr="40px"
         mb="5px"
       />
-      <Text fontSize="xl">Hello, Martyna</Text>
+      <Text fontSize="xl">Hello, Tymoteusz</Text>
       <Image
         src="https://cdn-icons-png.flaticon.com/512/3059/3059408.png"
         alt="menue"
@@ -27,7 +20,7 @@ const Navbar = () => {
         ml="auto"
       />
     </Box>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
