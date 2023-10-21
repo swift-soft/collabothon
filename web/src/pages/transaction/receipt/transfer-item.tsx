cad84ce562a83e2d03eec2cf7f192592207fbe5a
bnowacki <bknowacki@gmail.com>
2023-10-21 23:55:42 +0200

    done

--- web/src/pages/transaction/receipt/transfer-item.tsx
+++ web/src/pages/transaction/receipt/transfer-item.tsx
@@ -1,8 +1,14 @@
-import {Grid, GridItem} from '@chakra-ui/react'
+import React from 'react'
+
+import {CloseIcon} from '@chakra-ui/icons'
+import {Flex, Grid, GridItem, HStack, IconButton, Text} from '@chakra-ui/react'
+import {useDispatch} from 'react-redux'
 
 import {ReceiptItem} from '@/api/models'
+import {useAppSelector} from '@/store'
 import {formatMoney} from '@/utils/string'
 
+import {selectTransferItems, setTransferItems} from './state'
 import {TransferItem} from './types'
 import {getTransferAmount, settlementTypeToSymbol} from './utils'
 
@@ -12,22 +18,62 @@ type Props = {
   plus?: boolean
 }
 
-const TransferListItem = ({item, receiptItem, plus}: Props) => (
-  <Grid templateColumns="repeat(6, 1fr)" px={2}>
-    <GridItem colSpan={3}>
-      {item.user?.name?.split(' ')[0][0]}. {item.user?.name?.split(' ')[1]}
-    </GridItem>
-    <GridItem colSpan={2}>
-      {item.settlement_type === 'no_of_items' ? item.amount : formatMoney(item.amount)}
-      {!!item.settlement_type && settlementTypeToSymbol[item.settlement_type]}
-    </GridItem>
-    {receiptItem && (
-      <GridItem colSpan={1} textAlign="end">
-        {plus ? '+' : '-'}
-        {formatMoney(getTransferAmount(receiptItem, item))}
+const TransferListItem = ({item, receiptItem, plus}: Props) => {
+  const dispatch = useDispatch()
+  const transferItems = useAppSelector(selectTransferItems)
+
+  const handleDelete = React.useCallback(() => {
+    if (!receiptItem) return
+
+    const newValue = transferItems[receiptItem.name]?.filter((i) => i.user !== item.user)
+
+    const newTransferItems = {
+      ...transferItems,
+    }
+
+    if (!newValue?.length) {
+      delete newTransferItems[receiptItem.name]
+    } else {
+      newTransferItems[receiptItem.name] = newValue
+    }
+
+    dispatch(setTransferItems(newTransferItems))
+  }, [item, receiptItem, transferItems, dispatch])
+
+  return (
+    <Grid templateColumns="repeat(6, 1fr)" px={2} py={1}>
+      <GridItem colSpan={3}>
+        <HStack align="center">
+          <IconButton
+            aria-label="remove"
+            icon={<CloseIcon />}
+            variant="outline"
+            colorScheme="red"
+            rounded="full"
+            size="sm"
+            onClick={handleDelete}
+          />
+          <Text>
+            {item.user?.name?.split(' ')[0][0]}. {item.user?.name?.split(' ')[1]}
+          </Text>
+        </HStack>
       </GridItem>
-    )}
-  </Grid>
-)
+      <GridItem colSpan={2}>
+        <Flex align="center" h="full">
+          {item.settlement_type === 'no_of_items' ? item.amount : formatMoney(item.amount)}
+          {!!item.settlement_type && settlementTypeToSymbol[item.settlement_type]}
+        </Flex>
+      </GridItem>
+      {receiptItem && (
+        <GridItem colSpan={1} textAlign="end">
+          <Flex align="center" h="full">
+            {plus ? '+' : '-'}
+            {formatMoney(getTransferAmount(receiptItem, item))}
+          </Flex>
+        </GridItem>
+      )}
+    </Grid>
+  )
+}
 
 export default TransferListItem
