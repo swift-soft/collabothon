beb50a8fe0149f5dc07fdb61dda6f9cf4c4f183a
Tymoteusz Piwowarski <95983727+TymPiwowar@users.noreply.github.com>
2023-10-21 16:29:51 +0200

    created beautiful confirmation panel (#20)
    Co-authored-by: 247039 Tymoteusz Piwowarski <247039@edu.p.lodz.pl>

--- web/src/router/layout/index.tsx
+++ web/src/router/layout/index.tsx
@@ -1,4 +1,5 @@
 import {Box, Stack} from '@chakra-ui/react'
+import {askConfirmation} from '@common/components/transferm-notification-model'
 import {Outlet} from 'react-router'
 
 import {HEADER_HEIGHT, NAVBAR_HEIGHT} from '@/constants'
@@ -6,10 +7,12 @@ import {HEADER_HEIGHT, NAVBAR_HEIGHT} from '@/constants'
 import Header from './header'
 import Navbar from './navbar'
 
+
 const Layout = () => {
   return (
     <Stack minH="100vh">
       <Header />
+      
       <Box
         p={4}
         minH={`calc(100vh - ${NAVBAR_HEIGHT})`}
@@ -19,8 +22,10 @@ const Layout = () => {
         <Outlet />
       </Box>
       <Navbar />
+        
     </Stack>
   )
 }
+
 
 export default Layout
