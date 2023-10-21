4b85388a9d26035789b2385d6fc3a701bf21cc49
Wojciech Mokwiński <79480681+wmcqueensky@users.noreply.github.com>
2023-10-21 14:12:54 +0200

    Expenses cd (#19)

--- web/src/utils/string.ts
+++ web/src/utils/string.ts
@@ -1,29 +1,43 @@
-import {format} from 'date-fns'
+import {format, subDays, subMonths, subWeeks, subYears} from 'date-fns'
 import _ from 'lodash'
 
 export const sentenceCase = (input: string) => _.startCase(_.toLower(input))
 
-export const transactionDateFormat = 'dd  MMM | eeee'
+export const dateFormat = 'dd  MMM | eeee'
 export const formatTransactionDate = (value?: Date | string | null) =>
-  value ? format(new Date(value), transactionDateFormat) : ''
+  value ? format(new Date(value), dateFormat) : ''
 
-export const dateFormat = 'dd.MM.yyyy'
-export const formatDate = (value?: Date | string | null) => (value ? format(new Date(value), dateFormat) : '')
+export const polishDateFormat = 'dd.MM.yyyy'
+export const formatDate = (value?: Date | string | null) =>
+  value ? format(new Date(value), polishDateFormat) : ''
 
 export const polishTimestampFormat = 'dd.MM.yyyy HH:mm'
 export const formatTimestamp = (value: Date | string | null) =>
   value ? format(new Date(value), polishTimestampFormat) : ''
 
-export const dayDateFormat = 'DDDD, dd MMMM'
+export const dayDateFormat = 'eeee dd MMMM'
 export const formatDayStats = (value: Date | string | null) =>
   value ? format(new Date(value), dayDateFormat) : ''
 
-export const monthDateFormat = 'MMMM'
-export const formatMonthStats = (value: Date | string | null) =>
-  value ? format(new Date(value), monthDateFormat) : ''
+export const weekDateFormat = 'dd.MM'
+export const formatWeekStats = () => {
+  const today = new Date()
+  const weekBefore = subWeeks(today, 1)
+  return `${format(weekBefore, weekDateFormat)} to ${format(today, weekDateFormat)}`
+}
 
-export const yearDateFormat = 'yyyy'
-export const formatYearStats = (value: Date | string | null) =>
-  value ? format(new Date(value), yearDateFormat) : ''
+export const monthDateFormat = 'dd.MM'
+export const formatMonthStats = () => {
+  const today = new Date()
+  const monthBefore = subMonths(today, 1)
+  return `${format(monthBefore, monthDateFormat)} to ${format(today, monthDateFormat)}`
+}
+
+export const yearDateFormat = 'dd.MM.yyyy'
+export const formatYearStats = () => {
+  const today = new Date()
+  const yearBefore = subYears(today, 1)
+  return `${format(yearBefore, yearDateFormat)} - ${format(today, yearDateFormat)}`
+}
 
 export const formatMoney = (v?: number | null) => ((v || 0) / 100).toFixed(2)
