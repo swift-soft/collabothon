d1a3b84b4c1e5f03a75cf7ef7bd408f73f5ed22b
bnowacki <bknowacki@gmail.com>
2025-03-30 21:05:27 +0200

    some sql

--- sql_snippets/insert_transactions.sql
+++ sql_snippets/insert_transactions.sql
@@ -1,11 +1,22 @@
-INSERT INTO "transactions" ("amount", "created_at", "accounted_at", "source_account", "destination_account")
+create or replace function "get_random_transaction_title"()
+  returns text
+  language sql
+as $$
+  SELECT titles[floor(array_upper(titles, 1) * random() + 1)::int] FROM (
+      SELECT ARRAY[
+        'Grocery shopping', 'Restaurant', 'Żabka', 'Clothes&Co', 'Biedronka', 'Lidl'] AS titles
+    ) AS subquery
+$$;
+
+INSERT INTO "transactions" ("title", "amount", "created_at", "accounted_at", "source_account", "destination_account")
 SELECT
-    floor(random() * 99901 + 100) as "amount", -- Random amount between 100 and 100000
+    get_random_transaction_title(),
+    floor(random() * 49901 + 100) as "amount", -- Random amount between 100 and 100000
     now() - (random() * interval '30 days') as "created_at", -- Random created_at within the last 30 days
     now() - (random() * interval '30 days') as "accounted_at", -- Random accounted_at within the last 30 days
     '05103138650498282939298823' as "source_account", -- Keep the same source_account
-    '19606417273928987553747070' as "destination_account" -- Keep the same destination_account
-FROM generate_series(1, 100);
+    '53736385621521069198391732' as "destination_account" -- Keep the same destination_account
+FROM generate_series(1, 20);
 
 -- Update "accounted_at" to be exactly 10 hours after "created_at"
 UPDATE "transactions"
